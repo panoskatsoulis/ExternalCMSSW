03e896803f06769ba0ab578c0e23db5eddb91ea7
kpanos <panos.katsoulis@cern.ch>
2018-10-03 19:05:14 +0200

    Introduced new validation code - Withought matching for misordered muons

---
+++ MuonStudy/python/validationNew_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+validation = cms.EDAnalyzer(
+    "Validation",
+    system = cms.string("BMTF"),
+    phiHits = cms.InputTag("bmtfDigis"),
+    etaHits = cms.InputTag("bmtfDigis"),
+    muonsData = cms.InputTag("bmtfDigis:BMTF"),
+    muonsEmu = cms.InputTag("simBmtfDigis:BMTF")
+)
